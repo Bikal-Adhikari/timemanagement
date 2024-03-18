76acce9d8a55d56dc4f574d1c8570e6f8d13ce99
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-03-18 12:02:39 +1100

    Javascript for on submit form

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const taskList = [];
 
+const handleOnSubmit = (form) => {
+  const newForm = new FormData(form);
+
+  const task = newForm.get("task");
+  const hr = newForm.get("hr");
+  const obj = {
+    task,
+    hr,
+  };
+  taskList.push(obj);
+};
