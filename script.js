91497477126e76354502f5de039d498e59c77007
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-03-19 09:31:10 +1100

    version 1 Time management

--- script.js
+++ script.js
@@ -65,7 +65,9 @@ const displayBadList = () => {
 <td>${item.hr}hrs</td>
 <td class="text-end">
  
-  <button class="btn btn-warning btn-sm">
+  <button onclick="switchTask('${
+    item.id
+  }','entry')" class="btn btn-warning btn-sm">
     <i class="fa-sharp fa-solid fa-arrow-left-long"></i>
   </button>
   <button onclick="handOnDelete('${item.id}')" class="btn btn-danger btn-sm">
@@ -76,6 +78,12 @@ const displayBadList = () => {
   });
 
   badElm.innerHTML = str;
+
+  const ttlBad = temArg.reduce((acc, item) => {
+    return acc + item.hr;
+  }, 0);
+
+  document.getElementById("total-bad").innerText = ttlBad;
 };
 
 //[{task: "dd", hr:"88"}]
@@ -93,6 +101,7 @@ const handOnDelete = (id) => {
   if (window.confirm("Are you sure, you want to delete the item?")) {
     taskList = taskList.filter((item) => item.id !== id);
     display();
+    total();
   }
 };
 
@@ -118,94 +127,3 @@ const randomIdGenerator = () => {
   }
   return id;
 };
-
-// const display = () => {
-//   let str = ``;
-//   const tempArg = taskList.filter((item) => item.type === "entry");
-//   tempArg.forEach((item, i) => {
-//     str += `
-//     <tr>
-//     <th>${i + 1}</th>
-//     <td>${item.task}</td>
-//     <td>${item.hr}hrs</td>
-//     <td class="text-end">
-//       <button onclick="handelOnDelete('${
-//         item.id
-//       }')"class="btn btn-danger btn-sm">
-//         <i class="fa-solid fa-trash"></i>
-//       </button>
-//       <button onclick="switchTask('${
-//         item.id
-//       }', 'bad')" class="btn btn-success btn-sm">
-//         <i class="fa-solid fa-arrow-right-long"></i>
-//       </button>
-//     </td>
-//   </tr>`;
-//   });
-
-//   entryElm.innerHTML = str;
-//   displayBadList();
-// };
-// const displayBadList = () => {
-//   let str = ``;
-//   const tempArg = taskList.filter((item) => item.type === "bad");
-//   tempArg.forEach((item, i) => {
-//     str += `
-//     <tr>
-//     <th>${i + 1}</th>
-//     <td>${item.task}</td>
-//     <td>${item.hr}hrs</td>
-//     <td class="text-end">
-//       <button onclick="handelOnDelete('${
-//         item.id
-//       }')"class="btn btn-danger btn-sm">
-//         <i class="fa-solid fa-trash"></i>
-//       </button>
-//       <button class="btn btn-success btn-sm">
-//         <i class="fa-solid fa-arrow-right-long"></i>
-//       </button>
-//     </td>
-//   </tr>`;
-//   });
-
-//   entryElm.innerHTML = str;
-// };
-
-// const total = () => {
-//   //   let ttl = 0;
-//   //   taskList.forEach((item) => (ttl += eval(item.hr)));
-
-//   const ttl = taskList.reduce((acc, item) => {
-//     return acc + item.hr;
-//   }, 0);
-
-//   document.getElementById("ttlHrs").innerText = ttl;
-//   return ttl;
-// };
-
-// const handelOnDelete = (id) => {
-//   if (window.confirm("Are you sure you want to delete this item?")) {
-//     taskList = taskList.filter((item) => item.id !== id);
-//     display();
-//   }
-// };
-
-// const switchTask = (id, type) => {
-//   taskList = taskList.map((item) => {
-//     if (item.id === id) item.type = type;
-//     return item;
-//   });
-//   display();
-// };
-
-// const randomIdGenerator = () => {
-//   const idLength = 6;
-//   const str = "ASDFGHJKLQWERTYUIOPZXCVBNM1234567890asdfghjklzxcvbnmqwertyuiop";
-
-//   let id = "";
-//   for (let i = 0; i < idLength; i++) {
-//     const randomPosition = Math.floor(Math.random() * str.length);
-//     id += str[randomPosition];
-//   }
-//   return id;
-// };
